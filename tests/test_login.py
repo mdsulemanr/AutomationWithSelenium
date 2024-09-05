cf5870ff9b12d8d6a169dcd4de4abcff56bffd6a
mdsulemanr <mdsulemanr@gmail.com>
2024-09-05 12:31:52 +0500

    made use of config.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 from pages.login_page import LoginPage
+from config import BASE_URL, USERNAME, PASSWORD
 
 def test_valid_login():
     driver = webdriver.Chrome()
     login_page = LoginPage(driver)
 
-    # Open the login page
-    login_page.open_login_page()
+    # Open the login page using the base URL from config
+    login_page.open_login_page(BASE_URL)
 
-    # Perform login actions
-    login_page.enter_username("tomsmith")
-    login_page.enter_password("SuperSecretPassword!")
+    # Perform login actions using constants for username and password
+    login_page.enter_username(USERNAME)
+    login_page.enter_password(PASSWORD)
     login_page.click_login()
 
     # Verify the success message
